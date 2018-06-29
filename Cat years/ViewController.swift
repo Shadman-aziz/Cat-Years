141468a09357323b508069f03e4181007646a882
Shadman Aziz <97sdmn@gmail.com>
2018-06-29 11:27:49 -0400

    ff
    first app done

--- Cat years/ViewController.swift
+++ Cat years/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func button(_ sender: Any) {
+        if let age = textField.text {
+            if let ageAsNumber = Int(age) {
+                let ageInCatYears = ageAsNumber*7
+                displayLabel.text = "your cat is " + String(ageInCatYears) + " in cat years"
+        
+            
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
